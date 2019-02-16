746402b099d8a7e330a0cc79073ba43b01728fa0
Timothy Ip <ipt@flip3.engr.oregonstate.edu>
2019-02-16 11:34:22 -0800

    Add test helper fxn files and first working copy of randomtestadventurer.c. Makefile has appropriate rule additions

--- projects/ipt/dominion/randomtestadventurer.c
+++ projects/ipt/dominion/randomtestadventurer.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"
+#include "unittestFxn.h"
 
 #define DEBUG 0
 #define NOISY_TEST 1
@@ -11,8 +12,8 @@
 
 int main(argv, argc){
 	srand(NULL); 
-	 int i, n, r, p, deckCount, discardCount, handCount, numPlayers, currentPlayer; 
-	int[MAX_PLAYERS] oppHandCount, oppDeckCount, oppDiscardCount; 
+	int ii, deckCount, discardCount, handCount, numPlayers, currentPlayer, allPassed; 
+	int oppHandCount[MAX_PLAYERS], oppDeckCount[MAX_PLAYERS], oppDiscardCount[MAX_PLAYERS]; 
 	int k[10] = {adventurer, council_room, feast, gardens, mine,
 	       remodel, smithy, village, baron, great_hall};
 	struct gameState G; 
@@ -21,22 +22,24 @@ int main(argv, argc){
 	printf ("RANDOM TESTS.\n");
 
 
-
-	int currentPlayer; 
+	int count = 0; 
+	allPassed = 1; 
 	while(count < NUMTESTS){
-		numPlayers = rand() % 3 + 1; 
+		numPlayers = (rand() % 2) + 2; 
+		G.numPlayers = numPlayers;
 		currentPlayer = rand() % G.numPlayers; 
 		//Initialize a random game scenario to start w/
 		initializeGame(numPlayers, k, rand(), &G);
-		endTurn(G); 
+		endTurn(&G);  //End first players turn and then modify gamestate manually
+
 		//Randomize deck and hand counts for selected player
 		G.handCount[currentPlayer] = rand() % (MAX_HAND-2); //must hold 2 more drawn treasure
 		for (ii = 0; ii < G.handCount[currentPlayer]; ii++){
-			drawCard(currentPlayer, G);
+			drawCard(currentPlayer, &G);
 		}
 		G.deckCount[currentPlayer] = rand() % MAX_DECK; 
-		int cardPosition = rand()%G.handCount; 
-		G.hand[currentPlayer][cardPosition] = adventurer; 
+		int cardPosition = rand() % (G.handCount[currentPlayer]); 
+		G.hand[currentPlayer][cardPosition] = adventurer; //insert adventurer at cardPosition
 		G.whoseTurn = currentPlayer; 
 
 		//Remember states before playing card
@@ -52,7 +55,7 @@ int main(argv, argc){
 				oppDiscardCount[ii] = G.discardCount[ii]; 
 			}
 		}
-		supplyCounts[treasure_map+1]; 
+		int supplyCounts[treasure_map+1]; 
 		for(ii=0; ii < treasure_map+1; ii++){
 			supplyCounts[ii] = G.supplyCount[ii]; 
 		}
@@ -65,10 +68,50 @@ int main(argv, argc){
 		int treasure2 = (G.hand[G.whoseTurn][G.handCount[player]-1] == silver || G.hand[G.whoseTurn][G.handCount[player]-1]== gold || G.hand[G.whoseTurn][G.handCount[player]-1]); 
 		if(!assertFxn(treasure1, treasure2) && treasure1 == 1 ){ 
 			allPassed = 0; 
-			addedCards = G.handCount[G.whoseTurn] - handCount; 
+			int addedCards = G.handCount[G.whoseTurn] - handCount; 
 			printf("2 treasure cards were not added to the hand by the Adventurer card fxn. Last two cards treasure?: %i, %i\n", G.hand[G.whoseTurn][handCount+1], G.hand[G.whoseTurn][handCount+2]); 
 		}	
+
+		if(!assertFxn(playedCount+1, G.playedCardCount)){ 
+			allPassed = 0; 
+			printf("More or less than just the Adventurer card was played from the players hand. Expected = %i, actual = %i\n", playedCount+1, G.playedCardCount); 
+		}
+		//Check that at least 2 cards were removed from players deck 
+		if(deckCount-2 <= G.deckCount[G.whoseTurn] ){
+			allPassed = 0; 
+			printf("Players deck did not have at least 2 cards removed by Adventurer card fxn.\n"); 
+		}
+
+		//Check for no state change in other players. 
+		for(ii=1; ii < numPlayers; ii++){
+			if(ii != currentPlayer){
+				if(!assertFxn(oppDeckCount[ii], G.deckCount[ii] )){
+					allPassed = 0; 
+					printf("Other players deck was altered by Adventurer card fxn.\n"); 
+				}
+				if(!assertFxn(oppHandCount[ii], G.handCount[ii] )){
+					allPassed = 0; 
+					printf("Other players hand was altered by Adventurer card fxn.\n"); 
+				}
+				if(!assertFxn(oppDiscardCount[ii], G.discardCount[ii] )){
+					allPassed = 0; 
+					printf("Other players discard pile was altered by Adventurer card fxn.\n"); 
+				}
+			//Check for no state change in victory card piles and kingdom card piles
+				for(ii=0; ii < treasure_map+1; ii++){
+					if(!assertFxn(supplyCounts[ii], G.supplyCount[ii] )){
+						allPassed = 0; 
+						printf("Supply count %i was altered by Adventurer card fxn.\n", ii) ; 
+					}
+				}
+			}
+		}
+		printf("End of random run %i.\n", count); 
+		count++; 
 	}
+	char fxnName[] = {"Adventurer Card"}; 
+	passFail(allPassed, fxnName); 
+
 }
 
 /*struct gameState {
---
+++ projects/ipt/dominion/unittestFxn.h
@@ -0,0 +1,4 @@
+int passFail(int allPassed, char *fxnName); 
+
+int assertFxn(int test, int actual); 
+
